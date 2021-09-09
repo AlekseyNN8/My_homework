c73a6c1c9ca15e8524b8a618a40a8f14b8814ed2
AlekseyNN8 <kai.dlaser@mail.com>
2021-09-09 19:04:39 +0300

    homework2.3 in process

--- Homework2.3.py
+++ Homework2.3.py
@@ -1,12 +1,14 @@
 
-print('Введите число')
-number2 = input()
-import random
 
-q = random.randint(1, 100)
-if int(number2) < q:
-      print('Вы ввели число ' + number2 + ', которое меньше ' + str(q))
-elif int(number2) > q:
-     print('Вы ввели число ' + number2 + ', которое больше ' + str(q))
-else:
-     print('Вы ввели число ' + number2 + ', которое равно ' + str(q))
+
+
+IN Process
+loading ............... 10 %
+
+# print('Введите число')
+# number2 = input()
+# import random
+#
+# q = random.randint(1, 100)
+# if int(number2) < q:
+#       print('Вы ввели число ' + number2 + ', которое меньше ' + str(q))
