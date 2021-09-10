4fe8e2713756edc5a938d6a28e20ba4b002d42fd
AlekseyNN8 <kai.dlaser@mail.com>
2021-09-10 09:59:55 +0300

    Process complete

--- Homework2.2.py
+++ Homework2.2.py
@@ -6,7 +6,7 @@ import random
 
 q = random.randint(1, 100)
 if int(number2) < q:
-      print('Вы ввели число ' + number2 + ', которое меньше ' + str(q))
+     print('Вы ввели число ' + number2 + ', которое меньше ' + str(q))
 elif int(number2) > q:
      print('Вы ввели число ' + number2 + ', которое больше ' + str(q))
 else:
--- Homework2.3.py
+++ Homework2.3.py
@@ -2,13 +2,25 @@
 
 
 
-IN Process
-loading ............... 10 %
+# IN Process
+# loading ............... 10 %
 
-# print('Введите число')
-# number2 = input()
-# import random
-#
-# q = random.randint(1, 100)
-# if int(number2) < q:
-#       print('Вы ввели число ' + number2 + ', которое меньше ' + str(q))
+print('Введите число')
+number2 = input()
+import random
+
+q = random.randint(1, 100)
+w = random.randint(1, 100)
+if q > int(number2) and w > int(number2):
+    print('Вы ввели число ' + number2 + ', которое меньше ' + str(q) + ' и меньше ' + str(w))
+if q > int(number2) and w < int(number2):
+    print('Вы ввели число ' + number2 + ', которое меньше ' + str(q) + ' и больше ' + str(w))
+if q < int(number2) and w < int(number2):
+    print('Вы ввели число ' + number2 + ', которое больше ' + str(q) + ' и больше ' + str(w))
+if q < int(number2) and w > int(number2):
+    print('Вы ввели число ' + number2 + ', которое больше ' + str(q) + ' и меньше ' + str(w))
+if q == int(number2) and w == int(number2):
+    print('Ура! Вы ввели число ' + number2 + ', которое равно' + str(q) + ' и равно ' + str(w) + '/nВы супер герой!')
+
+
+
