a45619a9fb62de79df7d4e0380d58b98356bd3a1
AlekseyNN8 <kai.dlaser@mail.com>
2021-09-10 16:55:24 +0300

    homework done!

--- Homework2.3.py
+++ Homework2.3.py
@@ -19,8 +19,16 @@ if q < int(number2) and w < int(number2):
     print('Вы ввели число ' + number2 + ', которое больше ' + str(q) + ' и больше ' + str(w))
 if q < int(number2) and w > int(number2):
     print('Вы ввели число ' + number2 + ', которое больше ' + str(q) + ' и меньше ' + str(w))
+if q == int(number2) and w > int(number2):
+    print('Вы ввели число ' + number2 + ', которое равно ' + str(q) + ' и меньше ' + str(w))
+if q < int(number2) and w == int(number2):
+    print('Вы ввели число ' + number2 + ', которое больше ' + str(q) + ' и равно ' + str(w))
+if q == int(number2) and w < int(number2):
+    print('Вы ввели число ' + number2 + ', которое равно ' + str(q) + ' и больше ' + str(w))
+if q > int(number2) and w == int(number2):
+    print('Вы ввели число ' + number2 + ', которое меньше ' + str(q) + ' и равно ' + str(w))
 if q == int(number2) and w == int(number2):
-    print('Ура! Вы ввели число ' + number2 + ', которое равно' + str(q) + ' и равно ' + str(w) + '/nВы супер герой!')
+     print('Ура! Вы ввели число ' + number2 + ', которое равно' + str(q) + ' и равно ' + str(w) + 'Вы супер герой!')
 
 
 
